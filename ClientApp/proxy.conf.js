a5acd758dd51e72be57cf1ced2b6878d56d219cc
Bastiaan Stroosnijder <bas@strootje.com>
2024-03-11 14:55:19 +0100

    setup frontend with tailwind and connection to backend

--- ClientApp/proxy.conf.js
+++ ClientApp/proxy.conf.js
@@ -1,20 +1,21 @@
-const { env } = require('process');
+const { env } = require("process");
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'http://localhost:20543';
+const target = env.ASPNETCORE_HTTPS_PORT
+  ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}`
+  : env.ASPNETCORE_URLS
+  ? env.ASPNETCORE_URLS.split(";")[0]
+  : "https://localhost:7235";
 
 const PROXY_CONFIG = [
   {
-    context: [
-      "/weatherforecast",
-   ],
+    context: ["/api"],
     proxyTimeout: 10000,
     target: target,
     secure: false,
     headers: {
-      Connection: 'Keep-Alive'
-    }
-  }
-]
+      Connection: "Keep-Alive",
+    },
+  },
+];
 
 module.exports = PROXY_CONFIG;
--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public companies: Company[] = [];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Company[]>(`${baseUrl}api/v1/companies`).subscribe((companies) => {
+      this.companies = companies;
+    });
+  }
+}
+
+export interface Company {
+  id: number;
+  name: string;
+  address: Address;
+}
+
+export interface Address {
+  street: string;
+  city: string;
+  zipcode: string;
+  country: string;
 }
