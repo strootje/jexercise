94e4769dfad5369df5d43fe56535714ed8534f59
Bastiaan Stroosnijder <bas@strootje.com>
2024-03-11 23:09:10 +0100

    proper data seeding

--- Data/JexContext.cs
+++ Data/JexContext.cs
@@ -15,6 +15,7 @@ public class Company
 
 public class Address
 {
+   public int CompanyId { get; set; }
    public string? Street { get; set; }
    public string? City { get; set; }
    public string? Zipcode { get; set; }
@@ -40,18 +41,36 @@ public class JexContext(JexConfiguration configuration) : DbContext
 
    protected override void OnModelCreating(ModelBuilder builder)
    {
+      var jex = new Company { Id = 1, Name = "Jex" };
+      var koekela = new Company { Id = 2, Name = "Koekela" };
+      var bar3 = new Company { Id = 3, Name = "Bar 3" };
+
       builder.Entity<Company>(entity =>
       {
          entity.ToTable("Companies");
-         entity.OwnsOne(p => p.Address);
          entity.HasMany(p => p.JobOffers).WithOne(p => p.Company).HasForeignKey(p => p.CompanyId);
          entity.HasIndex(p => p.Name).IsUnique();
+
+         entity.HasData(jex, koekela, bar3);
+         entity.OwnsOne(p => p.Address).HasData(
+            new Address { CompanyId = jex.Id, Street = "Nassaukade 5", City = "Rotterdam", Zipcode = "3071 JL", Country = "Netherlands" },
+            new Address { CompanyId = koekela.Id, Street = "Nieuwe Binnenweg 79a", City = "Rotterdam", Zipcode = "3014 GB", Country = "Netherlands" },
+            new Address { CompanyId = bar3.Id, Street = "", City = "Rotterdam", Zipcode = "3014 GE", Country = "Netherlands" });
       });
 
       builder.Entity<JobOffer>(entity =>
       {
          entity.ToTable("JobOffers");
          entity.HasOne(p => p.Company).WithMany(p => p.JobOffers).HasForeignKey(p => p.CompanyId);
+
+         entity.HasData(
+            new JobOffer { Id = 1, Title = "Software Engineer", CompanyId = jex.Id, Description = "Turn Code into Coffee" },
+            new JobOffer { Id = 2, Title = "Cake Engineer", CompanyId = koekela.Id, Description = "Turn Cake into Coffee" },
+            new JobOffer { Id = 3, Title = "CEO", CompanyId = jex.Id, Description = "Feed the children of Rotterdam" },
+            new JobOffer { Id = 4, Title = "Bier Tapper", CompanyId = bar3.Id, Description = "Lekker biertjes tappen" },
+            new JobOffer { Id = 5, Title = "Afwasser", CompanyId = bar3.Id, Description = "Spelen in een sopje" },
+            new JobOffer { Id = 6, Title = "Fietscourier", CompanyId = koekela.Id, Description = "Uitkijken met die hobbels" },
+            new JobOffer { Id = 7, Title = "Test Engineer", CompanyId = jex.Id, Description = "Turn food for children into Tests" });
       });
    }
 
--- Migrations/20240311220751_Initial.cs
+++ Migrations/20240311220751_Initial.cs
@@ -2,6 +2,8 @@
 
 #nullable disable
 
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
 namespace jexercise.Migrations
 {
     /// <inheritdoc />
@@ -15,11 +17,11 @@ namespace jexercise.Migrations
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "INTEGER", nullable: false),
-                    Name = table.Column<string>(type: "TEXT", nullable: false),
-                    Address_Street = table.Column<string>(type: "TEXT", nullable: false),
-                    Address_City = table.Column<string>(type: "TEXT", nullable: false),
-                    Address_Zipcode = table.Column<string>(type: "TEXT", nullable: false),
-                    Address_Country = table.Column<string>(type: "TEXT", nullable: false)
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Address_Street = table.Column<string>(type: "TEXT", nullable: true),
+                    Address_City = table.Column<string>(type: "TEXT", nullable: true),
+                    Address_Zipcode = table.Column<string>(type: "TEXT", nullable: true),
+                    Address_Country = table.Column<string>(type: "TEXT", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -32,8 +34,8 @@ namespace jexercise.Migrations
                 {
                     Id = table.Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Sqlite:Autoincrement", true),
-                    Title = table.Column<string>(type: "TEXT", nullable: false),
-                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
                     CompanyId = table.Column<int>(type: "INTEGER", nullable: false)
                 },
                 constraints: table =>
@@ -45,6 +47,30 @@ namespace jexercise.Migrations
                         principalTable: "Companies",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "Address_City", "Address_Country", "Address_Street", "Address_Zipcode", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Rotterdam", "Netherlands", "Nassaukade 5", "3071 JL", "Jex" },
+                    { 2, "Rotterdam", "Netherlands", "Nieuwe Binnenweg 79a", "3014 GB", "Koekela" },
+                    { 3, "Rotterdam", "Netherlands", "", "3014 GE", "Bar 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "JobOffers",
+                columns: new[] { "Id", "CompanyId", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "Turn Code into Coffee", "Software Engineer" },
+                    { 2, 2, "Turn Cake into Coffee", "Cake Engineer" },
+                    { 3, 1, "Feed the children of Rotterdam", "CEO" },
+                    { 4, 3, "Lekker biertjes tappen", "Bier Tapper" },
+                    { 5, 3, "Spelen in een sopje", "Afwasser" },
+                    { 6, 2, "Uitkijken met die hobbels", "Fietscourier" },
+                    { 7, 1, "Turn food for children into Tests", "Test Engineer" }
                 });
 
             migrationBuilder.CreateIndex(
